c4704f054554fcc0715d216698d6181f78c15d94
Yehor Havryliuk <36529973+Jergs@users.noreply.github.com>
2025-04-06 20:51:42 +0300

    [#7] Add basic business entities

---
+++ src/main/resources/db/changelog/sql/V2025.04.06.16.45__initial_schema.sql
@@ -0,0 +1,30 @@
+-- liquibase formatted sql
+
+-- changeset yehorhavryliuk:1743946566986-5
+CREATE TABLE roles
+(
+    id BIGSERIAL PRIMARY KEY,
+    name VARCHAR(255) NOT NULL UNIQUE
+);
+
+-- changeset yehorhavryliuk:1743946566986-6
+CREATE TABLE users
+(
+    id BIGSERIAL PRIMARY KEY,
+    username   VARCHAR(255) NOT NULL UNIQUE,
+    email      VARCHAR(255) NOT NULL UNIQUE,
+    password   VARCHAR(255) NOT NULL,
+    created_at TIMESTAMP WITH TIME ZONE NOT NULL,
+    updated_at TIMESTAMP WITH TIME ZONE
+);
+
+-- changeset yehorhavryliuk:1743946566986-7
+CREATE TABLE users_roles
+(
+    role_id BIGINT NOT NULL,
+    user_id BIGINT NOT NULL,
+    PRIMARY KEY (user_id, role_id),
+    CONSTRAINT fk_user FOREIGN KEY (user_id) REFERENCES users (id),
+    CONSTRAINT fk_role FOREIGN KEY (role_id) REFERENCES roles (id)
+);
+
